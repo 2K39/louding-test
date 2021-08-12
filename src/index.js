aa5432cbc520ece0ec1f8619fc82db2e9e98136e
2K39 <ahmed.yousif.k@gmail.com>
2021-08-13 00:12:45 +0300

    louding

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { LoaderProvider, useLoading, BallTriangle } from '@agney/react-loading';
 
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
+ReactDOM.render(
+<LoaderProvider indicator={<BallTriangle width="50" />}>
+    <App/>
+</LoaderProvider>
+, document.querySelector("#root"));
